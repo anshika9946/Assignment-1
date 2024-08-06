418a2ab801433de04d192e11d029add91ff6f797
Anshika <anshika2011087@akgec.ac.in>
2024-08-06 13:28:36 +0530

    Home page button Setup

---
+++ lib/assignment2.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class Assignment2 extends StatelessWidget {
+  const Assignment2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Row & Column Example'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                color: const Color.fromARGB(255, 255, 61, 174),
+                width: 100,
+                height: 100,
+              ),
+            ],
+          ),
+          Container(
+            color: const Color.fromARGB(255, 10, 133, 86),
+            width: 100,
+            height: 100,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/assignment1.dart';
-// import 'package:my_app/assignment2.dart';
-// import 'package:my_app/assignment3.dart';
-// import 'package:my_app/assignment4.dart';
-// import 'package:my_app/assignment5.dart';
+import 'assignment1.dart';
+import 'assignment2.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Assignment 1',
-      home: Assignment1(),
+    return MaterialApp(
+      title: 'Flutter Assignments',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Assignments'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Assignment1()),
+                );
+              },
+              child: const Text('Assignment 1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Assignment2()),
+                );
+              },
+              child: const Text('Assignment 2'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
